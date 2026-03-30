b79138e5fc436f1843e8e080648bcff6b203b282
kawserali-64 <kawserali6436@gmail.com>
2026-03-30 14:57:41 +0600

    get started added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Banner from './Component/Banner/Banner'
 import Footer from './Component/Footer/Footer'
+import GetSteps from './Component/GetSteps/GetSteps'
 import NavBar from './Component/NavBar/NavBar'
 import PricingCard from './Component/PricingCard/PricingCard'
 import Rating from './Component/Rating/Rating'
@@ -14,6 +15,7 @@ function App() {
       <NavBar></NavBar>
       <Banner></Banner>
       <Rating></Rating>
+      <GetSteps></GetSteps>
       <PricingCard></PricingCard>
       <Workflow></Workflow>
       <Footer></Footer>
---
+++ src/Component/GetSteps/GetSteps.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import userImg from '../../assets/user.png'
+import productImg from '../../assets/package.png'
+import rocketImg from '../../assets/rocket.png'
+
+const GetSteps = () => {
+    return (
+        <div className='mt-32 bg-[#F9FAFC] p-20'>
+             <div className='text-center mb-8'>
+                <h1 className='font-bold text-black text-4xl '>Get Started in 3 Steps</h1>
+                <p className='text-[#627382] mt-4'>Start using premium digital tools in minutes, not hours.</p>
+            </div> 
+            <div className='grid grid-cols-3 container mx-auto'>
+
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full w-8 h-8 flex items-center justify-center ml-auto mr-6 mt-6'>
+                    <p className='text-white'>01</p>
+                </div>
+                <figure className="px-10 pt-10">
+                    <div className='bg-purple-100 rounded-full w-20 h-20 flex items-center justify-center'>
+                    <img
+                        src={userImg}
+                        alt="Shoes"
+                        className="rounded-xl items-center" />
+                    </div>
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Create Account</h2>
+                    <p>Sign up for free in seconds. No credit card <br /> required to get started.</p>
+                    
+                </div>
+            </div>
+
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full w-8 h-8 flex items-center justify-center ml-auto mr-6 mt-6'>
+                    <p className='text-white'>02</p>
+                </div>
+                <figure className="px-10 pt-10">
+                    <div className='bg-purple-100 rounded-full w-20 h-20 flex items-center justify-center'>
+                    <img
+                        src={productImg}
+                        alt="Shoes"
+                        className="rounded-xl items-center" />
+                    </div>
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Choose Products</h2>
+                    <p>Browse our catalog and select the tools <br /> that fit your needs.</p>
+                    
+                </div>
+            </div>
+
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full w-8 h-8 flex items-center justify-center ml-auto mr-6 mt-6'>
+                    <p className='text-white'>03</p>
+                </div>
+                <figure className="px-10 pt-10">
+                    <div className='bg-purple-100 rounded-full w-20 h-20 flex items-center justify-center'>
+                    <img
+                        src={rocketImg}
+                        alt="Shoes"
+                        className="rounded-xl items-center" />
+                    </div>
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Start Creating</h2>
+                    <p>Download and start using your premium <br /> tools immediately.</p>
+                    
+                </div>
+            </div>
+
+        </div>
+        </div>
+    );
+};
+
+export default GetSteps;
